dffce75de696d924294ca5f6bf5a13e747b6eb2b
srinath-3898 <srinath3898@gmail.com>
2024-01-08 12:23:57 +0530

    changed base url

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  // baseURL: "https://app.starbuzz.space/api/v2",
-  baseURL: "https://nodev2.starbuzz.ai/api/v2",
+  baseURL: "https://app.starbuzz.space/api/v2",
+  // baseURL: "https://nodev2.starbuzz.ai/api/v2",
   // baseURL: "http://10.50.64.123:8080/api/v2",
 });
 
